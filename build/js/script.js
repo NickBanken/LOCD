54a51c4753c3724fdcfe300527b989962603f52f
Nick <nick.banken@gmail.com>
2021-01-18 15:30:55 +0100

    update build V0.1.0

--- build/js/script.js
+++ build/js/script.js
@@ -1,127 +1,142 @@
-let max = document.getElementById('max');
-let min = document.getElementById('min');
+//################## Minimize and maximize ReadMe ##################
+let resizeButtons = () => {
+  let maxIcon = document.getElementById("max");
+  let minIcon = document.getElementById("min");
+  let parent = document.getElementById("insertData");
 
-let editorView = document.querySelector('.editor');
-let instructionsView = document.querySelector('.instructions');
+  //handle if we user is looking at big screen/not styelsheet is a global variable in the index.html
+  maxIcon.addEventListener("click", () => {
+    maxIcon.style.display = "none";
+    minIcon.style.display = "inline-block";
+    sessionStorage.setItem("maxScreen", true);
+    stylesheet.disabled = false;
+  });
 
-let input = document.querySelector('.editor__input');
-let output = document.querySelector('.editor__output');
-let main = document.querySelector('main');
+  minIcon.addEventListener("click", () => {
+    maxIcon.style.display = "inline-block";
+    minIcon.style.display = "none";
+    sessionStorage.removeItem("maxScreen");
+    stylesheet.disabled = true;
+  });
 
-let viewport = document.getElementById('viewport');
-let insertView = document.querySelector('#insertData');
+  //when the user scrolls the min/max icon follows along.
+  parent.addEventListener("scroll", function (e) {
+    let y = parent.scrollTop;
+    let topCo = y + 30;
+    maxIcon.style.top = `${topCo}px`;
+    minIcon.style.top = `${topCo}px`;
+  });
+};
 
-max.addEventListener("click", ()=>{
-	max.style.display = "none";
-	min.style.display = "inline-block";
-	stylesheet.disabled = false;
-	sessionStorage.setItem('maxScreen', true);
-})
+// When in max screen, the user can use 2 buttons to navigate from the instructions to the editor
+let viewportNavigation = () => {
+  let editorView = document.querySelector(".editor");
+  let instructionsView = document.querySelector(".instructions");
 
+  document
+    .getElementById("instructionsButton")
+    .addEventListener("click", () => {
+      instructionsView.classList.remove("instructions--max");
+      editorView.classList.remove("editor--max");
+    });
 
-min.addEventListener("click", ()=>{
-	max.style.display = "inline-block";
-	min.style.display = "none";
+  document.getElementById("editorButton").addEventListener("click", () => {
+    instructionsView.classList.add("instructions--max");
+    editorView.classList.add("editor--max");
+  });
+};
 
-	sessionStorage.removeItem('maxScreen');
-	stylesheet.disabled = true;
-})
+//################## Dropdown code ##################
+let setDropdown = () => {
+  let dropdownBtn = document.querySelector(".dropdown__btn");
 
-let inputView = document.getElementById('inputTab').children;
+  dropdownBtn.addEventListener("click", showDropDown);
 
+  function showDropDown() {
+    document.getElementById("dropdownList").classList.toggle("show");
+    let currentEl = document.querySelector(".current");
+    currentEl.scrollIntoView(true);
+  }
 
-for(let i = 0; i < inputView.length; i++){
-	let result = inputView[i];
-	console.log(result)
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function (event) {
+    if (!event.target.matches(".dropdown__btn")) {
+      let dropdown = document.getElementById("dropdownList");
+      if (dropdown.classList.contains("show")) {
+        dropdown.classList.remove("show");
+      }
+    }
+  };
+};
+
+//################## Arrow navigation ##################
+
+let setupArrowNav = () =>{
+  // Find all links in the dropdown menu and create the previouse and next function out of it
+  let links = document.querySelectorAll(".dropdown__list ul > li a");
+  for (let i = 0; i < links.length; i++) {
+    // added + "index.html" because in hugo they open the folder and read the file.
+    if (
+      JSON.stringify(links[i].href + "index.html").includes(
+        window.location.pathname
+      )
+    ) {
+      setNavArrow(links, i - 1, ".prev");
+      setNavArrow(links, i + 1, ".next");
+      links[i].classList.add("current");
+      break;
+    }
+  }
+
+  //Set the arrow navigation for all arrows on the screen.
+  function setNavArrow(arr, index, query) {
+    let arrowArr = document.querySelectorAll(query);
+    if (!arr[index]) {
+      // for each arrow element that doesn't have a link, we disable them.
+      arrowArr.forEach((el) => {
+        el.classList.add("disabled");
+      });
+    } else {
+      // add the url to each element out there.
+      arrowArr.forEach((el) => {
+        el.href = arr[index];
+      });
+    }
+  }
 }
 
 
-let maxIcon = document.getElementById("max");
-let minIcon = document.getElementById("min");
-let parent = document.getElementById("insertData");
-
-
-parent.addEventListener('scroll', function(e) {
-	let y = parent.scrollTop;
-	let topCo = y + 30;
-	maxIcon.style.top = `${topCo}px`;
-	minIcon.style.top = `${topCo}px`;
-})
-
-document.getElementById("instructionsButton").addEventListener("click",()=>{
-	instructionsView.classList.remove("instructions--max")
-	editorView.classList.remove("editor--max")
-})
-
-document.getElementById("editorButton").addEventListener("click",()=>{
-	instructionsView.classList.add("instructions--max")
-	editorView.classList.add("editor--max")
-})
-
-
-
-//Navigation code
-
-let btn = document.querySelector('.dropdown__btn');
-
-btn.addEventListener('click',myFunction)
-
-function myFunction() {
-	document.getElementById("dropdownList").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-	if (!event.target.matches('.dropdown__btn')) {
-	  var dropdowns = document.getElementsByClassName("dropdown__list");
-	  var i;
-	  for (i = 0; i < dropdowns.length; i++) {
-		var openDropdown = dropdowns[i];
-		
-		if (openDropdown.classList.contains('show')) {
-		  openDropdown.classList.remove('show');
-		}
-	  }
-	}
-  }
-
-//################## Arrow navigation ###################
-
-  let links = document.querySelectorAll(".dropdown__list ul > li a");
-
-	for(let i=0; i<links.length; i++){
-		// added + "index.html" because on the server they open the folder and read the file.
-		if(JSON.stringify(links[i].href+"index.html").includes(window.location.pathname)){
-			console.log(links[i].href , window.location.pathname)
-			setNavArrow(links,i-1,".prev")
-			setNavArrow(links,i+1,".next")
-			links[i].classList.add("current")
-			break;
-		}
-	}
-
-
-	function setNavArrow(arr,index,query){
-		let arrowArr = document.querySelectorAll(query);
-		if(!arr[index]){
-			arrowArr.forEach(el=>{
-				el.classList.add("disabled");
-			})
-		}else{
-			arrowArr.forEach(el=>{
-				el.href = arr[index];
-			})
-		}
-	}
-
 // burger menu toggle
-let burgerMenu = document.getElementById("menu__btn");
-const burgerNav = document.querySelector(".nav__menu--hamburger")
+let setupBurger = () =>{
+  let burgerMenu = document.getElementById("menu__btn");
+  const burgerNav = document.querySelector(".nav__menu--hamburger");
+  
+  burgerMenu.addEventListener("change", function () {
+    if (this.checked) {
+      burgerNav.classList.add("burger--active");
+    } else {
+      burgerNav.classList.remove("burger--active");
+    }
+  });
+}
 
-burgerMenu.addEventListener("change",function(){
-	if(this.checked){
-		burgerNav.classList.add("burger--active")
-	}else{
-		burgerNav.classList.remove("burger--active")
-	}	
-})
+
+let init = () => {
+  resizeButtons();
+  viewportNavigation();
+  setDropdown();
+  setupArrowNav();
+  setupBurger();
+};
+
+init();
+
+// looping through the html,css and js viewtabs WIP
+// let inputView = document.getElementById('inputTab').children;
+
+// for(let i = 0; i < inputView.length; i++){
+// 	let result = inputView[i];
+// 	if (result.tagName !== "BUTTON"){
+// 		console.log(result)
+// 	}
+// }
